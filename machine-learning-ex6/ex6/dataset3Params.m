e8897a0882b0b6119019c92dab5c1b141ea6befc
rohansapre <11bit025@nirmauni.ac.in>
2016-06-22 14:56:10 +0530

    finished exercise 6 and week 7

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -7,6 +7,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
+x1 = [1 2 1]; x2 = [0 4 -1];
+testValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+minError = 100;
+
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
@@ -23,11 +27,18 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+for i=1:length(testValues)
+	for j=1:length(testValues)
+		model= svmTrain(X, y, testValues(i), @(x1, x2) gaussianKernel(x1, x2, testValues(j)));
+		predictions = svmPredict(model, Xval);
+		predError = mean(double(predictions ~= yval));
+		if predError < minError
+			minError = predError;
+			C = testValues(i);
+			sigma = testValues(j);
+		end
+	end
+end
 
 % =========================================================================
 
